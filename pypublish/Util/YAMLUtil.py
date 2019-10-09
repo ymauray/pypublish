f181f160086881d96eb101d96e7d6abc26bafeb1
Yannick Mauray <yannick.mauray@arcantel.ch>
2019-10-09 14:49:39 +0200

    Reduced visibility

--- pypublish/Util/YAMLUtil.py
+++ pypublish/Util/YAMLUtil.py
@@ -8,31 +8,31 @@ class YAMLUtil(object):
         dirname = os.path.abspath(os.path.dirname(yaml_file))
         with open(yaml_file) as stream:
                 config = yaml.safe_load(stream)
-                config = YAMLUtil.process_includes(config, dirname, level)
+                config = YAMLUtil.__process_includes(config, dirname, level)
                 if level == 0:
                     refconfig = None
                     while refconfig != config:
                         refconfig = config
-                        config = YAMLUtil.replace_tokens(config)
+                        config = YAMLUtil.__replace_tokens(config)
                 return config
 
     @staticmethod
-    def replace_tokens(config):
-        config = YAMLUtil.replace_tokens_internal(config, config)
+    def __replace_tokens(config):
+        config = YAMLUtil.__replace_tokens_internal(config, config)
         return config
 
     @staticmethod
-    def replace_tokens_internal(value, config):
+    def __replace_tokens_internal(value, config):
         if isinstance(value, dict):
             output = {}
             for k in list(value):
                 v = value[k]
-                output[k] = YAMLUtil.replace_tokens_internal(v, config)
+                output[k] = YAMLUtil.__replace_tokens_internal(v, config)
             return output
         elif isinstance(value, list):
             output = []
             for item in list(value):
-                output.append(YAMLUtil.replace_tokens_internal(item, config))
+                output.append(YAMLUtil.__replace_tokens_internal(item, config))
             return output
         elif isinstance(value, str):
             p = re.compile(r"\${([^}]+)}", re.IGNORECASE)
@@ -51,7 +51,7 @@ class YAMLUtil(object):
             sys.exit()
 
     @staticmethod
-    def process_includes(config, dirname, level):
+    def __process_includes(config, dirname, level):
         output = {}
         for key in list(config):
             if key == 'include':
